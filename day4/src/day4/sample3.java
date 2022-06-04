acc606d9765c02528c503f2868a41d234b0fbadf
HeeMang28 <22tkakd@naver.com>
2022-06-04 23:37:30 +0900

    call by value

---
+++ day4/src/day4/sample3.java
@@ -0,0 +1,23 @@
+package day4;
+
+/* class Updater {
+	void update(Counter counter) {
+		counter.count++;
+	}
+}
+
+class Counter {
+	int count = 0; // 객체변수
+} 
+public class sample3 {
+	public static void main(String[] args) {
+		Counter myCounter = new Counter();
+		System.out.println("before update: " + myCounter.count);
+		Updater myUpdater = new Updater();
+		myUpdater.update(myCounter);
+		System.out.println("after update: " + myCounter.count);
+		//before update:0
+		//ater update:1 결과 값 나옴
+	}
+
+} */
