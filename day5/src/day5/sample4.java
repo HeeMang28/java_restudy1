3cc517367fbf1e1ebcb8d3e49e06c7d0a3975c30
HeeMang28 <22tkakd@naver.com>
2022-06-06 20:10:32 +0900

    생성자

---
+++ day5/src/day5/sample4.java
@@ -0,0 +1,36 @@
+package day5;
+
+/* class Animal {
+    String name;
+
+    void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Dog extends Animal {
+    void sleep() {
+        System.out.println(this.name + " zzz");
+    }
+}
+
+class HouseDog extends Dog {
+    HouseDog(String name) {
+        this.setName(name);
+    }
+
+    void sleep() {
+        System.out.println(this.name + " zzz in house");
+    }
+
+    void sleep(int hour) {
+        System.out.println(this.name + " zzz in house for " + hour + " hours");
+    }
+}
+
+public class sample4 {
+	public static void main(String[] args) {
+    	HouseDog dog = new HouseDog();
+        System.out.println(dog.name); // null 출력
+       }
+   } */
